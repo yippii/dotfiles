c2a9935efd4c6f7c95de90121efc405356c8cba4
yippii <liboris20@gmail.com>
2026-04-06 09:03:45 -0400

    Bug fixes 2.0

--- nixos/modules/base/desktop/steam.nix
+++ nixos/modules/base/desktop/steam.nix
@@ -1,33 +1,35 @@
 { inputs, self, ... }: {
-  flake.nixosModules.desktop = { pkgs, ... }: {
-    programs.steam = {
-      enable = true;
-      gamescopeSession.enable = true;
+  flake.nixosModules.desktop = { lib, pkgs, ... }: lib.mkMerge [
+    (lib.mkIf pkgs.stdenv.isx86_64 {
+      programs.steam = {
+        enable = true;
+        gamescopeSession.enable = true;
 
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
+        extraCompatPackages = with pkgs; [
+          proton-ge-bin
+        ];
+
+        extraPackages = with pkgs; [
+          SDL2
+          gamescope
+          er-patcher
+        ];
+
+        protontricks.enable = true;
+      };
+
+      programs.gamemode.enable = true;
+
+      programs.nix-ld = {
+        enable = true;
+        libraries = pkgs.steam-run.args.multiPkgs pkgs;
+      };
+
+      environment.systemPackages = with pkgs; [
+        mangohud
+        inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-lazer-bin
+        inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.wine-discord-ipc-bridge
       ];
-
-      extraPackages = with pkgs; [
-        SDL2
-        gamescope
-        er-patcher
-      ];
-
-      protontricks.enable = true;
-    };
-
-    programs.gamemode.enable = true;
-
-    programs.nix-ld = {
-      enable = true;
-      libraries = pkgs.steam-run.args.multiPkgs pkgs;
-    };
-
-    environment.systemPackages = with pkgs; [
-      mangohud
-      inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-lazer-bin
-      inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.wine-discord-ipc-bridge
-    ];
-  };
+    })
+  ];
 }
--- nixos/modules/hosts/NixOS-Mac/configuration.nix
+++ nixos/modules/hosts/NixOS-Mac/configuration.nix
@@ -80,7 +80,7 @@
     # and migrated your data accordingly.
     #
     # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-    system.stateVersion = "25.11"; # Did you read the comment?
+    system.stateVersion = "26.05"; # Did you read the comment?
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
