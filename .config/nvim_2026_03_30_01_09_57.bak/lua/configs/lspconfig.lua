eb401e3ff31eaf28a1cb1198f92d75a813cadd4b
yippii <liboris20@gmail.com>
2026-04-06 04:30:05 -0400

    Bombed a bunch of cache

--- .config/nvim_2026_03_30_01_09_57.bak/lua/configs/lspconfig.lua
+++
@@ -1,6 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "html", "cssls", "java_language_server", "nixd" }
-vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 
--- .config/nvim_2026_04_01_15_35_57.bak/lua/options.lua
+++
@@ -1,7 +0,0 @@
-require "nvchad.options"
-
--- add yours here!
-vim.g.clipboard = 'wl-copy'
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
