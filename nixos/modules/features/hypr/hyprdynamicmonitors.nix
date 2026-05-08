ae1301d395d3fac8f0beb104dd9b63056242aa86
yippii <liboris20@gmail.com>
2026-05-07 22:40:21 -0400

    Updated .gitignore

--- nixos/modules/features/hypr/hyprdynamicmonitors.nix
+++ nixos/modules/features/hypr/hyprdynamicmonitors.nix
@@ -15,9 +15,81 @@
     services.hyprdynamicmonitors = {
       enable = true;
       mode = "user";
-      configFile = lib.mkMerge [
-        (lib.mkIf pkgs.stdenv.isx86_64 "/home/yippie/.config/hyprdynamicmonitors/config-nig.toml")
-        (lib.mkIf pkgs.stdenv.isAarch64 "/home/yippie/.config/hyprdynamicmonitors/config-mac.toml")
+      config = lib.mkMerge [
+        (lib.mkIf pkgs.stdenv.isx86_64 ''
+          [general]
+          destination = "$HOME/.config/hypr/monitors.conf"
+
+          [power_events]
+
+          [power_events.dbus_query_object]
+          path = "/org/freedesktop/UPower/devices/line_power_AC"
+
+          [[power_events.dbus_signal_match_rules]]
+          object_path = "/org/freedesktop/UPower/devices/line_power_AC"
+
+
+          [profiles.Laptop]
+          config_file = "/home/yippie/.config/hyprdynamicmonitors/hyprconfigs_P1/Laptop.go.tmpl"
+          config_file_type = "template"
+          [profiles.Laptop.conditions]
+
+          [[profiles.Laptop.conditions.required_monitors]]
+          description = "BOE 0x0AE0"
+          monitor_tag = "monitor0"
+
+
+          [profiles.Desktop]
+          config_file = "/home/yippie/.config/hyprdynamicmonitors/hyprconfigs_P1/Desktop.go.tmpl"
+          config_file_type = "template"
+          [profiles.Desktop.conditions]
+
+          [[profiles.Desktop.conditions.required_monitors]]
+          description = "BOE 0x0AE0"
+          monitor_tag = "monitor0"
+
+          [[profiles.Desktop.conditions.required_monitors]]
+          description = "LG Electronics LG ULTRAGEAR 408NTCZ3F411"
+          monitor_tag = "monitor1"
+
+        '')
+
+        (lib.mkIf pkgs.stdenv.isAarch64 ''
+          [general]
+          destination = "$HOME/.config/hypr/monitors.conf"
+
+          [power_events]
+
+          [power_events.dbus_query_object]
+          path = "/org/freedesktop/UPower/devices/line_power_AC"
+
+          [[power_events.dbus_signal_match_rules]]
+          object_path = "/org/freedesktop/UPower/devices/line_power_AC"
+
+
+          [profiles.Laptop]
+          config_file = "/home/yippie/.config/hyprdynamicmonitors/hyprconfigs/Laptop.go.tmpl"
+          config_file_type = "template"
+          [profiles.Laptop.conditions]
+
+          [[profiles.Laptop.conditions.required_monitors]]
+          description = "BOE 0x0AE0"
+          monitor_tag = "monitor0"
+
+
+          [profiles.Desktop]
+          config_file = "/home/yippie/.config/hyprdynamicmonitors/hyprconfigs/Desktop.go.tmpl"
+          config_file_type = "template"
+          [profiles.Desktop.conditions]
+
+          [[profiles.Desktop.conditions.required_monitors]]
+          description = "BOE 0x0AE0"
+          monitor_tag = "monitor0"
+
+          [[profiles.Desktop.conditions.required_monitors]]
+          description = "LG Electronics LG ULTRAGEAR 408NTCZ3F411"
+          monitor_tag = "monitor1"
+        '')
       ];
     };
   };
