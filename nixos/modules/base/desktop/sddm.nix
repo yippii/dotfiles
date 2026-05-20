51af0f175f3d8ff727ccef199c1eb45f67933d2a
Boris Li <litszwaiboris@yahoo.com>
2026-05-20 10:46:40 -0400

    Updated login screen

--- nixos/modules/base/desktop/sddm.nix
+++ nixos/modules/base/desktop/sddm.nix
@@ -8,52 +8,32 @@
     pkgs,
     ...
   }: {
-    services.displayManager = {
-      sddm = {
-        enable = true;
-
-        extraPackages = with pkgs; [
-          kdePackages.qtsvg
-          kdePackages.qtmultimedia
-          kdePackages.qtvirtualkeyboard
-          kdePackages.qt5compat
-          kdePackages.layer-shell-qt
-        ];
-
-        theme = "sddm-astronaut-theme";
-
-        wayland = {
-          enable = true;
-          compositorCommand = "${lib.getExe' pkgs.kdePackages.kwin "kwin_wayland"} --drm --no-lockscreen --no-global-shortcuts --locale1";
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.cage}/bin/cage -s -d -- gtkgreet-launch";
+          user = "yippie";
         };
-
-        settings = {
-          General = {
-            GreeterEnvironment = lib.concatStringsSep "," [
-              "QT_WAYLAND_SHELL_INTEGRATION=layer-shell"
-            ];
-          };
-
-          Theme = {
-            # Both of these are nessecary otherwise the cursor isn't shown at all
-            CursorTheme = "catppuccin-macchiato-dark-cursors";
-            CursorSize = 24;
-          };
-        };
-      };
-
-      autoLogin = {
-        enable = false;
-        user = "yippie";
       };
     };
 
-    environment.systemPackages = with pkgs; [
-      (sddm-astronaut.override {
-        embeddedTheme = "hyprland_kath";
-      })
+    environment.etc."greetd/environments".text = ''
+      uwsm start -e -D Hyprland hyprland.desktop
+      Hyprland
+      fish
+      bash
+    '';
 
-      catppuccin-cursors.macchiatoDark
+    environment.systemPackages = with pkgs; [
+      (writeShellApplication {
+        name = "gtkgreet-launch";
+        runtimeInputs = [gtkgreet wlr-randr];
+        text = ''
+          wlr-randr --output eDP-1 --scale 2
+          gtkgreet
+        '';
+      })
     ];
   };
 }
