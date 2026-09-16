1b6d00965b3896ecf966e570e192cfc669b455a9
Boris Li <liboris20@gmail.com>
2026-09-16 18:02:20 +0000

    NixOS:

--- nixos/modules/base/impermanance/disko.nix
+++ nixos/modules/base/impermanance/disko.nix
@@ -23,49 +23,49 @@
       devices.disk.main = {
         device = "/dev/nvme0n1";
         type = "disk";
-      };
 
-      content.type = "gpt";
+        content.type = "gpt";
 
-      content.partitions.esp = {
-        name = "Boot";
-        size = "2G";
-        type = "EF00";
+        content.partitions.esp = {
+          name = "Boot";
+          size = "2G";
+          type = "EF00";
 
-        content = {
-          type = "filesystem";
-          format = "vfat";
-          mountpoint = "/boot";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+          };
         };
-      };
 
-      content.partitions.swap = {
-        name = "Swap";
-        size = "4G";
+        content.partitions.swap = {
+          name = "Swap";
+          size = "4G";
 
-        content = {
-          type = "swap";
-          resumeDevice = true;
+          content = {
+            type = "swap";
+            resumeDevice = true;
+          };
         };
-      };
 
-      content.partitions.root = {
-        name = "NixOS";
-        size = "100%";
+        content.partitions.root = {
+          name = "NixOS";
+          size = "100%";
 
-        content = {
-          type = "btrfs";
-          extraArgs = ["-f"];
+          content = {
+            type = "btrfs";
+            extraArgs = ["-f"];
 
-          subvolumes = {
-            "/persistent" = {
-              mountOptions = ["subvol=persistent" "noatime"];
-              mountpoint = "/persistent";
-            };
+            subvolumes = {
+              "/persistent" = {
+                mountOptions = ["subvol=persistent" "noatime"];
+                mountpoint = "/persistent";
+              };
 
-            "/nix" = {
-              mountOptions = ["subvol=nix" "noatime"];
-              mountpoint = "/nix";
+              "/nix" = {
+                mountOptions = ["subvol=nix" "noatime"];
+                mountpoint = "/nix";
+              };
             };
           };
         };
